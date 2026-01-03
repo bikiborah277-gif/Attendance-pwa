be6e651395cbba5f8acb06dbcba11f3bc282b63c
Chinmay Kashyap <bikiborah277@gmail.com>
2026-01-03 20:38:43 +0530

    new

--- service-worker.js
+++ service-worker.js
@@ -7,24 +7,12 @@ const FILES = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(FILES))
-  );
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener("activate", e=>{
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
